368c6258602b1031ee168cfbdeb5d343f8350459
jobpartal <Aaddii_xe@live.com>
2020-08-07 18:27:35 +0500

    backend with get and post operation

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,28 @@ router.get("/jobs", function (req, res, next) {
     });
 });
 
+router.get("/job/:sid", function (req, res, next) {
+  Job.findById(req.params.sid).exec(function (error, results) {
+    if (error) {
+      return next(error);
+    }
+    // Respond with valid data
+    res.json(results);
+  });
+});
+//POST Operations
+router.post("/addjob", function (req, res, next) {
+  Job.create(req.body)
+    .then(
+      (job) => {
+        console.log("Student has been Added ", job);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(job);
+      },
+      (err) => next(err)
+    )
+    .catch((err) => next(err));
+});
+
 module.exports = router;
